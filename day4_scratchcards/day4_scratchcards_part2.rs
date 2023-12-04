1d1ec5dd56a8ffe3b7cdfa4e1a4ed5af55ad8102
David Cooper <david.cooper+git@djac.au>
2023-12-04 14:03:33 +0800

    Typo.

--- day4_scratchcards/day4_scratchcards_part2.rs
+++ day4_scratchcards/day4_scratchcards_part2.rs
@@ -6,11 +6,11 @@ fn main()
     match std::fs::read_to_string(&file)
     {
         Err(err) => println!("Cannot read '{file}': {err}"),
-        Ok(content) => scratchcards_part1(&content)
+        Ok(content) => scratchcards_part2(&content)
     }
 }
 
-fn scratchcards_part1(content: &str)
+fn scratchcards_part2(content: &str)
 {
     let mut sum = 0;
     let mut copy_stack = VecDeque::from(vec![1; 1000]);
