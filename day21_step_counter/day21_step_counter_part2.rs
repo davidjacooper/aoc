52e33e6840de646e1cf790b5e6607f929fd2b551
David Cooper <david.cooper+git@djac.au>
2023-12-21 21:35:40 +0800

    Day 21, part 2, after much struggle.

--- day21_step_counter/day21_step_counter_part2.rs
+++ day21_step_counter/day21_step_counter_part2.rs
@@ -1,36 +1,5 @@
-/* Fun facts
- *
- * The input dimensions are 131 x 131, with the starting position dead centre.
- * (There are 65 squares up, down, left and right of the start.)
- *
- * There are no rocks at all in either the row or column of the starting position.
- *
- * When we try to fill adjacent copies of the board, the fill pattern will be reversed, because the dimensions are odd.
- *
- * The origin board can be completely filled with 7481 plots. It _could_ be completely filled with 7407 plots if
- * the pattern were reversed.
- *
- * With only 64 steps (the inner diamond):
- * - we get 3666 vs 3489
- *
- * 26501365 - 65 evenly divides 131 (by 2 * 2 * 5 * 5 * 7 * 17 * 17).
- *
- * => after 26501365 steps, the area spans (26501365 - 65) / 131 = 202300 extra boards in each direction,
- * and the total vertical/horizontal span is thus (202300 * 2 + 1) = 404601.
- *
- * Also, the
- *
- * Taking a smaller example:
- * with 196 steps (65 + 131), we ought to reach the outer edges of the four boards surrounding the origin board.
- *
- *
- */
-
-
-
 use std::time::Instant;
-use std::collections::HashSet;
-
+use std::collections::{HashSet,HashMap};
 
 fn main()
 {
@@ -41,7 +10,7 @@ fn main()
         Ok(content) =>
         {
             let start = Instant::now();
-            if let Err(err) = step_counter_part1(&content)
+            if let Err(err) = step_counter_part2(&content)
             {
                 println!("ERROR: {}", err);
             }
@@ -50,156 +19,123 @@ fn main()
     }
 }
 
-#[derive(Copy,Clone,Eq,PartialEq,Debug)]
-enum Square { Rock, Passable, PartiallyReachable, ExactlyReachable }
-use Square::*;
+const TOTAL_STEPS: usize = 26501365;
 
-
-fn step_counter_part1(content: &str) -> Result<(), &str>
+fn step_counter_part2(content: &str) -> Result<(),&str>
 {
-    let mut start_i: usize = 0;
-    let mut start_j: usize = 0;
-
-    let mut grid: Vec<Vec<Square>> =
+    let mut start_i: i64 = 0;
+    let mut start_j: i64 = 0;
+    let grid: Vec<Vec<bool>> =
         content
         .trim_end()
         .lines()
         .enumerate()
         .map(|(i, line)|
-        {
-            let mut row: Vec<Square> =
-                line
-                .chars()
-                .enumerate()
-                .map(|(j, ch)|
+            line
+            .chars()
+            .enumerate()
+            .map(|(j, ch)|
+            {
+                if ch == 'S'
                 {
-                    if ch == 'S'
-                    {
-                        start_i = i;
-                        start_j = j;
-                    }
-                    if ch != '#' { Passable } else { Rock }
-                })
-                .collect();
-            row.insert(0, Rock);
-            row.push(Rock);
-            row
-        })
+                    start_i = i as i64;
+                    start_j = j as i64;
+                }
+                ch != '#'
+            })
+            .collect()
+        )
         .collect();
 
-    let width = grid[0].len();
-    grid.insert(0, vec![Rock; width]);
-    grid.push(vec![Rock; width]);
-
-    // // To get the diamond in the middle
-    // const MAX_STEPS: usize = 65;
-
-    // To get the entire block
-    const MAX_STEPS: usize = 131;
-
-
-    // // To get the top-left triangle
-    // (start_i, start_j) = (0, 0);
-    // const MAX_STEPS: usize = 64;
-    // // (look at the inverted output)
-
-    // // To get the top-right triangle
-    // (start_i, start_j) = (0, 130);
-    // const MAX_STEPS: usize = 64;
-    // // (look at the inverted output)
-
-    // // To get the bottom-left triangle
-    // (start_i, start_j) = (130, 0);
-    // const MAX_STEPS: usize = 64;
-    // // (look at the inverted output)
-
-    // // To get the bottom-right triangle
-    // (start_i, start_j) = (130, 130);
-    // const MAX_STEPS: usize = 64;
-    // // (look at the inverted output)
-
-
-    // // To get the square with the bottom-right cut-off
-    // (start_i, start_j) = (0, 0);
-    // const MAX_STEPS: usize = 195;
-    // // (look at the normal output)
-
-    // // To get the square with the bottom-left cut-off
-    // (start_i, start_j) = (0, 130);
-    // const MAX_STEPS: usize = 195;
-    // // (look at the normal output)
-
-    // // To get the square with the top-right cut-off
-    // (start_i, start_j) = (130, 0);
-    // const MAX_STEPS: usize = 195;
-    // // (look at the normal output)
-
-    // // To get the square with the top-left cut-off
-    // (start_i, start_j) = (130, 130);
-    // const MAX_STEPS: usize = 195;
-    // // (look at the normal output)
-
-
-    // // To get the top corner
-    // const MAX_STEPS: usize = 130;
-    // (start_i, start_j) = (130, 65);
-
-    // // To get the right corner
-    // const MAX_STEPS: usize = 130;
-    // (start_i, start_j) = (65, 0);
-
-    // // To get the bottom corner
-    // const MAX_STEPS: usize = 130;
-    // (start_i, start_j) = (0, 65);
-
-    // // To get the left corner
-    // const MAX_STEPS: usize = 130;
-    // (start_i, start_j) = (65, 130);
-
-    // Account for boundary.
-    start_i += 1;
-    start_j += 1;
-
-    println!("start=({start_i},{start_j})");
+    let mut explored = HashMap::<(i64,i64),bool>::new();
+    let mut current = HashSet::<(i64,i64)>::new();
+    current.insert((start_i, start_j));
 
     let mut n_reachable_exactly = 0;
     let mut n_reachable_partially = 0;
-    grid[start_i][start_j] =
-        if MAX_STEPS % 2 == 0
+    explored.insert(
+        (start_i, start_j),
+        if TOTAL_STEPS % 2 == 0
         {
             n_reachable_exactly = 1;
-            ExactlyReachable
+            true
         }
         else
         {
             n_reachable_partially = 1;
-            PartiallyReachable
-        };
+            false
+        });
 
-    let mut current = HashSet::<(usize,usize)>::new();
-    let mut next = HashSet::<(usize,usize)>::new();
-    current.insert((start_i, start_j));
+    let initial_steps = (grid.len() - 1) / 2;
+    let step_step = grid.len() * 2;
 
-    for n_steps in 0..MAX_STEPS
+    let mut n_steps = initial_steps;
+    let mut prev_n_steps = 0;
+
+    // Compute values for solving quadratic equation:
+    let mut points = Vec::<i64>::new();
+    for _ in 0..3
     {
-        let even = (MAX_STEPS - n_steps) % 2 == 0;
+        run(prev_n_steps..n_steps,
+            &grid,
+            &mut explored,
+            &mut current,
+            &mut n_reachable_exactly,
+            &mut n_reachable_partially);
+        points.push(n_reachable_exactly);
+
+        prev_n_steps = n_steps;
+        n_steps += step_step;
+    }
+
+    // Solve quadratic equation:
+    let a = points[2] / 2 - points[1] + points[0] / 2;
+    let b = -points[2] / 2 + 2 * points[1] - 3 * points[0] / 2;
+    let c = points[0];
+    let n = ((TOTAL_STEPS - initial_steps) / step_step) as i64;
+
+    let plots = a * n.pow(2) + b * n + c;
+    println!("#plots = {plots}");
+
+    Ok(())
+}
+
+
+fn run(steps: std::ops::Range<usize>,
+       grid: &Vec<Vec<bool>>,
+       explored: &mut HashMap<(i64,i64),bool>,
+       current: &mut HashSet<(i64,i64)>,
+       n_reachable_exactly: &mut i64,
+       n_reachable_partially: &mut i64)
+{
+    let height = grid.len() as i64;
+    let width = grid[0].len() as i64;
+
+    let mut next = HashSet::<(i64,i64)>::new();
+    let n_steps = steps.end;
+    for n in steps
+    {
+        let even = (TOTAL_STEPS - n) % 2 == 0;
 
         for (i, j) in current.drain()
         {
             for (i2, j2) in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]
             {
-                if grid[i2][j2] == Passable
+                if grid[i2.rem_euclid(height) as usize][j2.rem_euclid(width) as usize]
+                    && !explored.contains_key(&(i2, j2))
                 {
-                    grid[i2][j2] = if even
-                    {
-                        n_reachable_partially += 1;
-                        PartiallyReachable
-                    }
-                    else
-                    {
-                        n_reachable_exactly += 1;
-                        ExactlyReachable
-                    };
+                    explored.insert(
+                        (i2, j2),
+                        if even
+                        {
+                            *n_reachable_partially += 1;
+                            false
+                        }
+                        else
+                        {
+                            *n_reachable_exactly += 1;
+                            true
+                        });
                     next.insert((i2,j2));
                 }
             }
@@ -210,34 +146,51 @@ fn step_counter_part1(content: &str) -> Result<(), &str>
             current.insert(pos);
         }
     }
-
-    print_grid(&grid);
-    println!("#garden plots reachable in exactly {MAX_STEPS} steps = {n_reachable_exactly}");
-    println!("#garden plots reachable but not exactly = {n_reachable_partially}");
-
-    Ok(())
+    println!("#plots reachable after {} steps: {n_reachable_exactly} ({n_reachable_partially})", n_steps);
 }
 
-
-fn print_grid(grid: &Vec<Vec<Square>>)
+fn print_grid(grid: &Vec<Vec<bool>>, explored: &HashMap<(i64,i64),bool>)
 {
-    for (i, row) in grid.iter().enumerate()
+    let mut min_i = i64::MAX;
+    let mut min_j = i64::MAX;
+    let mut max_i = i64::MIN;
+    let mut max_j = i64::MIN;
+    for (i, j) in explored.keys()
     {
-        for (j, square) in row.iter().enumerate()
+        min_i = min_i.min(*i);
+        min_j = min_j.min(*j);
+        max_i = max_i.max(*i);
+        max_j = max_j.max(*j);
+    }
+
+    let height = grid.len() as i64;
+    let width = grid[0].len() as i64;
+
+    for i in (min_i - 2)..=(max_i + 2)
+    {
+        for j in (min_j - 2)..=(max_j + 2)
         {
+            let grid_i = i.rem_euclid(height);
+            let grid_j = j.rem_euclid(width);
+
+
             print!("{}{}\x1b[m",
-                   match (i, j)
+                   if grid_i == 0 || grid_i == (height - 1) ||
+                      grid_j == 0 || grid_j == (width - 1)
                    {
-                       (66, 66) => "\x1b[46m",
-                       (66, _ ) => "\x1b[44m",
-                       (_,  66) => "\x1b[42m",
-                       _        => ""
-                   },
-                   match square {
-                       Rock                          => "#",
-                       Passable                      => ".",
-                       PartiallyReachable            => " ",
-                       ExactlyReachable              => "\x1b[31;1mO\x1b[m" });
+                       "\x1b[44m"
+                   }
+                   else if grid_i == 65 || grid_j == 65
+                   {
+                       "\x1b[42m"
+                   }
+                   else { "" },
+                   match explored.get(&(i, j))
+                   {
+                       Some(true) => "\x1b[31;1mO",
+                       Some(false) => "\x1b[31m.",
+                       None => if grid[grid_i as usize][grid_j as usize] { "." } else { "#" }
+                   });
         }
         println!();
     }
